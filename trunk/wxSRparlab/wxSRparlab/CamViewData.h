88bf3bcb60c9108d12c18fd635ba3fd07c31428a
murej <murej@606b6968-d9a8-11de-87ae-b9033455ee04>
2007-11-09 16:38:45 +0000

    using class template for camera views; the dataArray is uberAllocated to double in order to smoothly avoid templatization of whole class

--- trunk/wxSRparlab/wxSRparlab/CamViewData.h
+++ trunk/wxSRparlab/wxSRparlab/CamViewData.h
@@ -35,7 +35,7 @@ private:
 	int AllocLUT(); /* initialize LUT */
 	int AllocRGBA(); /* initialize RGB and Alpha buffer */
 	int AllocDataArray(); /* initialize data buffer */
-
+	
 public:
 	/* Opening and closing */
 	void CloseView(wxCommandEvent& WXUNUSED(event)); //!< Closes the display
@@ -57,7 +57,59 @@ public:
 	void TextChangedDispMax(wxCommandEvent& WXUNUSED(event));/* */
 	
 	/* Copying data to display */
-	int SetUshortData( unsigned short * buf, int numPix);
+	template<typename T> int SetDataArray( T* buf, int numPix)
+	{
+		int res= 0;
+		if(buf == NULL){return -1;};
+		if(numPix*sizeof(T) > m_nDataWidth * m_nDataHeight *sizeof(T)){return -2;};
+		if(m_pDataArray == NULL){return -3;};
+
+		memcpy( (void*) m_pDataArray, (const void*) buf, numPix*sizeof(T));
+		T first = buf[0];
+		CamViewData::Map2rgb<T>(first);
+		CamViewData::m_DrawPanel->Refresh();
+		CamViewData::m_DrawPanel->Update();
+		CamViewData::Refresh();
+
+		return res;
+	};
+//template int CamViewData::SetDataArray<unsigned short>( unsigned short* buf, int numPix);
+	int SetUshortData( unsigned short * buf, int numPix); 
+
+
+	template<typename T> int Map2rgb(T firstVal)
+{
+	int res = 0;
+	if(m_pLUT       == NULL){return -1;};
+	if(m_pRGB       == NULL){return -2;};
+	if(m_pDataArray == NULL){return -3;};
+
+	T* data = (T*) m_pDataArray;
+	double invDyn = 1.0/ (m_dDispMax - m_dDispMin) * ( (double) m_nLUTlen);
+
+	int val = 0;
+	unsigned char *curPix, *curCol;
+	for(int i = 0 ; i <(m_nDataWidth * m_nDataHeight); i++)
+	{
+		val = (int)floor(( ((double)data[i]) - m_dDispMin ) * invDyn );
+		if(val <0){ val = 0;}
+		if(val > (m_nLUTlen-1) ) {val = (m_nLUTlen-1) ;}
+
+
+		curPix = &m_pRGB[m_nComp*i+0];
+		curCol = &m_pLUT[m_nComp*val];
+		memcpy( (void*) curPix, (const void*) curCol, m_nComp);
+	}
+
+	// convert data from raw image to wxImg 
+	m_pWxImg = wxImage( m_nDataWidth, m_nDataHeight, m_pRGB, TRUE );
+	m_pWxImg.SetAlpha( m_pAlpha, TRUE);
+	// convert to bitmap to be used by the window to draw
+	m_pBitmap = wxBitmap( m_pWxImg.Scale(m_nDataWidth, m_nDataHeight) );
+
+	return res;
+};
+
 
 private:
 
