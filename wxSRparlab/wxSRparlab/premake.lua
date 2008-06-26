2820c2d631b2b6dbae538ea733d30d72ab83965d
murej <murej@606b6968-d9a8-11de-87ae-b9033455ee04>
2008-06-26 17:07:08 +0000

    updated premake.lua Debug works Release still has linker problems

--- wxSRparlab/wxSRparlab/premake.lua
+++ wxSRparlab/wxSRparlab/premake.lua
@@ -1,6 +1,6 @@
 package.name = "wxSRparlabPREM"
 package.guid = "F0174772-C16D-4CED-8E0E-3B1348DB3630"
-package.kind = "exe"
+package.kind = "winexe"
 package.language = "c++"
 
 package.config["Debug"].bindir = string.format('%s%s',os.getenv("JMU_BUILDS"), "/Debug/bin")
@@ -22,18 +22,43 @@ package.excludes = {
 }
 --]]
 
-package.buildflags = {"no-pch"}
-package.defines = {""}
+package.buildflags = {"no-pch","no-main"}
+--package.defines = {""}
 package.includepaths = {
   string.format('%s%s',os.getenv("WXWIN"), "/include"),
   string.format('%s%s',os.getenv("WXWIN"), "/include/msvc")
 }
 package.libpaths = {
-  string.format('%s%s',os.getenv("WXWIN"), "/lib/vc_dl"),
+  string.format('%s%s',os.getenv("WXWIN"), "/lib/vc_dll"),
   string.format('%s%s',os.getenv("WXWIN"), "/lib/vc_lib"),
 }
 
 tinsert(package.config["Release"].buildflags, {"optimize-speed"})
 
-tinsert(package.config["Debug"].defines , "DEBUG")
-tinsert(package.config["Release"].defines , "NDEBUG")
+
+--addoption("--with-shared-wxwin", "link wxWidgets as a shared library")
+-- --Configure a C/C++ package to use wxWidgets
+--function configureWxWidgets(package)
+
+if (package.kind == "dll") then
+  tinsert(package.defines, "WXMAKINGDLL")
+end
+
+if (OS == "linux") then
+  tinsert(package.buildoptions, "`wx-config --cxxflags` -W -Wall -ansi -pedantic")
+  tinsert(package.linkoptions,  "`wx-config --libs`")
+end
+
+if (OS == "windows") then
+  --tinsert(package.defines, { "WIN32", "_WINDOWS", "WINVER=0x400", "_MT", "wxUSE_GUI=1" })
+  tinsert(package.links,   { "comctl32", "rpcrt4", "winmm", "advapi32", "wsock32"})
+
+  --tinsert(package.config["Debug"].defines , "_DEBUG")
+  tinsert(package.config["Release"].defines, "NDEBUG")
+
+  --tinsert(package.defines, "WXUSINGDLL")
+  tinsert(package.config["Debug"].links, {"wxmsw28d_core", "wxbase28d"})
+  tinsert(package.config["Release"].links, {"wxmsw28_core", "wxbase28"})
+end
+
+---end
