c811ad04c9868cff56a76401d8b2e93a2a3231ad
muredubo <muredubo@606b6968-d9a8-11de-87ae-b9033455ee04>
2009-05-13 12:09:02 +0000

    trying to unify different versions of srBuf.h

---
+++ trunk/wxSRparlab/srBuf.h
@@ -0,0 +1,20 @@
+/*
+ * srBuf.h
+ * A convenience structure to define a srBuffer (amp and phase),
+ * along with number of rows and columns
+ *
+ * @author: James Mure-Dubois
+ * @version: 2008.10.30
+ */
+
+#ifndef SRBUFHEADER
+#define SRBUFHEADER
+typedef struct srBufVar{
+    unsigned short* amp;
+    unsigned short* pha;
+    int nCols;
+    int nRows;
+    int bufferSizeInBytes;
+} SRBUF;
+#endif // 
+
