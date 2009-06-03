330ae16918c308fc48bd4666179791f0932e9a16
james <james@606b6968-d9a8-11de-87ae-b9033455ee04>
2009-06-03 22:48:53 +0000

    added premake4 files, which produce correct dylibs on mac not done for wxSRparlab

---
+++ trunk/wxSRparlab/premake4.lua
@@ -0,0 +1,34 @@
+--*	NOTES:
+--*		- use the '/' slash for all paths.
+--*****************************************************************************
+solution "wxSRparlabPREM"
+	configurations { "Debug", "Release" }
+
+-- This is for including other Premake scripts.
+--dofile( "wxSRparlabPREM_prem4.lua" )
+
+-- Add options here.
+newoption
+{
+	trigger = "dynamic-runtime",
+	description = "Use the dynamicly loadable version of the runtime."
+}
+
+newoption
+{
+	trigger = "unicode",
+	description = "Use the Unicode character set"
+}
+
+newoption
+{
+	trigger = "use-vtk",
+	description = "Enable building against VTK(tm) for rendering"
+}
+
+if _OPTIONS["use-vtk"] then
+	dofile( "wxSRparlab/wxSRparlabVTK_prem4.lua" )
+else
+	dofile( "wxSRparlab/wxSRparlab_prem4.lua" )
+end
+
