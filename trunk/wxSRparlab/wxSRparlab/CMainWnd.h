d20473944fde2ce968d2655bd5617f217414ceaa
james <james@606b6968-d9a8-11de-87ae-b9033455ee04>
2009-06-08 19:18:15 +0000

    had to modify handling of wxString for VTK file prefix conversino to char according to : http://wiki.wxwidgets.org/WxString#Converting_a_wxString_to_a_Normal_String + suppressed overloaded function for setVisVtk and setColVtk in order to avoid compile error ( Mac only :-/ , windows did not compile) of wxEventHandler not able to cast result

--- trunk/wxSRparlab/wxSRparlab/CMainWnd.h
+++ trunk/wxSRparlab/wxSRparlab/CMainWnd.h
@@ -104,8 +104,8 @@ private:
 	std::vector<wxCheckBox*> _visSegmVtk;		//!< list of SEGMENTED visibility checkboxes
 	std::vector<wxComboBox*> _colSegmVtk;		//!< list of SEGMENTED vtk color options
 	wxGridBagSizer *_sizerCamVisCol;
-	void SetVisVtk(std::vector<wxCheckBox*> visChkBox, std::vector<CamFrame*> camFrm, int idx); //!< react on event changed visibility
-	void SetColVtk(std::vector<wxComboBox*> colCmbBox, std::vector<CamFrame*> camFrm, int idx);  //!< react on event changed colormap
+	void SetVisVtkIdx(std::vector<wxCheckBox*> visChkBox, std::vector<CamFrame*> camFrm, int idx); //!< react on event changed visibility
+	void SetColVtkIdx(std::vector<wxComboBox*> colCmbBox, std::vector<CamFrame*> camFrm, int idx);  //!< react on event changed colormap
 	wxCheckBox* _ckParaProj; //! Parallel projection button
 	wxTimer	_renderTimer; //! timer for rendering events
 	int _rendTgt; //! target rendering time in ms
